75f378f74c36538a437501397b98a2d77ad7b936
Ryan Barrar <Rbarrar17@gmail.com>
2016-04-26 16:42:56 -0400

    wrote a recursive factorial algorithm in JavaScript

---
+++ algorithms/recursive_factorial.js
@@ -0,0 +1,16 @@
+var factorial = function(n) {
+	// base case:
+	if (n === 0){
+	    return 1;
+	}
+	// recursive case:
+	return factorial(n-1)*n;
+};
+
+println("The value of 0! is " + factorial(0) + ".");
+println("The value of 5! is " + factorial(5) + ".");
+
+Program.assertEqual(factorial(0), 1);
+Program.assertEqual(factorial(5), 120);
+Program.assertEqual(factorial(6), 720);
+Program.assertEqual(factorial(3), 6);
