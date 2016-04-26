5c490f2ded76b79279239d6f4e7096a834f8b7a9
Ryan Barrar <Rbarrar17@gmail.com>
2016-04-26 15:13:13 -0400

    fixed insert sort algorithm to sort entire array

--- algorithms/insert_sort.rb
+++ algorithms/insert_sort.rb
@@ -5,8 +5,16 @@ def insert(array, right_index, value)
     index -= 1
   end
   array[index+1] = value
+end
+
+def insertion_sort(array)
+  i = 1
+  while (i < array.length)
+    insert(array, i-1, array[i])
+    i += 1
+  end
   p array
 end
 
 array = [3, 5, 7, 11, 13, 2, 9, 6]
-insert(array, 4, 2)
+insertion_sort(array)
